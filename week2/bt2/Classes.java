7e8f8ab9a6aaa681177cc4e5962deba2b56ee783
ngochieu2003 <104835138+ngochieu2003@users.noreply.github.com>
2024-09-19 05:21:59 +0700

    Add files via upload

---
+++ week2/bt2/Classes.java
@@ -0,0 +1,10 @@
+public class Classes {
+    int x = 5;
+  
+    public static void main(String[] args) {
+      Classes myObj1 = new Classes ();  // Object 1
+      Classes myObj2 = new Classes();  // Object 2
+      System.out.println(myObj1.x);
+      System.out.println(myObj2.x);
+    }
+  }
